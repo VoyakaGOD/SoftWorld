aff9b73a04c6d12ffec03d0f51606f14c09757be
VoyakaGOD <ab3000645@gmail.com>
2024-03-07 16:51:33 +0300

    update Physics/physicalbody.h

--- Physics/physicalbody.h
+++ Physics/physicalbody.h
@@ -7,9 +7,11 @@
 class PhysicalBody
 {
 public:
-    const QRect &GetBoundingRect() const = 0;
+    QRect &GetBoundingRect() const = 0;
     void SolveCollision(PhysicalBody &another) = 0;
-    void KeepSceneBorders(const QRect &worlRect) = 0;
+    void KeepSceneBorders(const QRect &world_rect) = 0;
+    void ApplyInternalRestrictions(double delta_time) = 0;
+    void ApplyGravity(double air_density, double g, double delta_time) = 0;
 };
 
 #endif // PHYSICALBODY_H
