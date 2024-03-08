a0a76ab85bc10f1ef83ab515a340040b5051a06d
VoyakaGOD <ab3000645@gmail.com>
2024-03-08 15:23:10 +0300

    added mutex to SoftScene

--- Physics/softscene.cpp
+++ Physics/softscene.cpp
@@ -1,15 +1,19 @@
 #include "softscene.h"
 
-SoftScene::SoftScene(QRect world_rect, double air_density, double g):
+SoftScene::SoftScene(const QRect &world_rect, double air_density, double g):
     world_rect(world_rect), air_density(air_density), g(g) {}
 
 void SoftScene::Draw(QPainter &painter) const
 {
+    lock_guard<mutex> lock(synchronizer);
+
     throw "not implemented yet!";
 }
 
 void SoftScene::DoNextStep(double delta_time)
 {
+    lock_guard<mutex> lock(synchronizer);
+
     for(auto body1 : bodies)
     {
         body1->ApplyGravity(air_density, g, delta_time);
@@ -31,16 +35,22 @@ void SoftScene::DoNextStep(double delta_time)
 
 void SoftScene::AddBody(PhysicalBody *body)
 {
+    lock_guard<mutex> lock(synchronizer);
+
     bodies.push_back(body);
 }
 
 void SoftScene::RemoveBody(PhysicalBody *body)
 {
+    lock_guard<mutex> lock(synchronizer);
+
     bodies.remove(body);
 }
 
 InspectableParamsList SoftScene::GetInspectableParams()
 {
+    lock_guard<mutex> lock(synchronizer);
+
     InspectableParamsList list("scene", 2);
     list.params.push_back(InspectableDouble("air density", air_density));
     list.params.push_back(InspectableDouble("g", g));
@@ -49,10 +59,22 @@ InspectableParamsList SoftScene::GetInspectableParams()
 
 PhysicalBody *SoftScene::GetBodyAt(const QPoint &point) const
 {
+    lock_guard<mutex> lock(synchronizer);
+
     for(auto body : bodies)
     {
         if(body->ContainsPoint(point))
             return body;
     }
     return nullptr;
+}
+
+void SoftScene::Lock()
+{
+    synchronizer.lock();
+}
+
+void SoftScene::Unlock()
+{
+    synchronizer.unlock();
 }
