e2b62491570496ba648fa979a5c210e4402630d5
VoyakaGOD <ab3000645@gmail.com>
2024-03-11 05:02:22 +0300

    INSPECTABLE_NUMERIC

--- Inspector/inspectableparam.h
+++ Inspector/inspectableparam.h
@@ -26,9 +26,21 @@ public:                                                                         
     Inspectable##NAME(const char *name, TYPE &value) : name(name), value(value) { this->type = InspectableParamType::NAME; }    \
 }
 
-INSPECTABLE(int, Int);
-INSPECTABLE(double, Double);
-INSPECTABLE(float, Float);
+#define INSPECTABLE_NUMERIC(TYPE, NAME, MIN_VALUE, MAX_VALUE)                                                                   \
+class Inspectable##NAME : public InspectableParam                                                                               \
+{                                                                                                                               \
+public:                                                                                                                         \
+    const char *name;                                                                                                           \
+    TYPE min;                                                                                                                   \
+    TYPE max;                                                                                                                   \
+    TYPE &value;                                                                                                                \
+    Inspectable##NAME(const char *name, TYPE &value) : name(name), value(value), min(MIN_VALUE), max(MAX_VALUE)                 \
+    { this->type = InspectableParamType::NAME; }                                                                                \
+}
+
+INSPECTABLE_NUMERIC(int, Int, INT_MIN, INT_MAX);
+INSPECTABLE_NUMERIC(float, Float, 1e35, -1e35);
+INSPECTABLE_NUMERIC(double, Double, 1e305, -1e305);
 INSPECTABLE(QColor, Color);
 
 #undef INSPECTABLE
